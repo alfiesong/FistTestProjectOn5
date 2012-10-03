81eb07b7eeebada933c55f79e7d8fa6c6868fd33
ningyu song <UFO@ningyu-songmatoMacBook-Pro.local>
2012-10-02 22:11:03 -0500

    save

---
+++ FistTestProjectOn5/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  FistTestProjectOn5
+//
+//  Created by ningyu song on 10/2/12.
+//  Copyright (c) 2012 ningyu song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ FistTestProjectOn5/ThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThirdViewController.m
+//  FistTestProjectOn5
+//
+//  Created by ningyu song on 10/2/12.
+//  Copyright (c) 2012 ningyu song. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
